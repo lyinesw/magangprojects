05cba1479a01616ca9ad4b086b9a8f0e0cd51227
lyinesw <arlineeeee23@gmail.com>
2026-04-07 14:39:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -136,12 +136,12 @@ function terbilang(n) {
   
   if (n < 12) return bilangan[n];
   if (n < 20) return terbilang(Math.floor(n - 10)) + " belas";
-  if (n < 100) return terbilang(Math.floor(n / 10)) + " puluh" + terbilang(n % 10);
-  if (n < 200) return "seratus" + terbilang(n - 100);
-  if (n < 1000) return terbilang(Math.floor(n / 100)) + " ratus" + terbilang(n % 100);
-  if (n < 2000) return "seribu" + terbilang(n - 1000);
-  if (n < 1000000) return terbilang(Math.floor(n / 1000)) + " ribu" + terbilang(n % 1000);
-  if (n < 1000000000) return terbilang(Math.floor(n / 1000000)) + " juta" + terbilang(n % 1000000);
+  if (n < 100) return terbilang(Math.floor(n / 10)) + " puluh " + terbilang(n % 10);
+  if (n < 200) return "seratus" + (n > 100 ? " " + terbilang(n - 100) : "");
+  if (n < 1000) return terbilang(Math.floor(n / 100)) + " ratus " + terbilang(n % 100);
+  if (n < 2000) return "seribu " + terbilang(n - 1000);
+  if (n < 1000000) return terbilang(Math.floor(n / 1000)) + " ribu" + (n % 1000 > 0 ? " " + terbilang(n % 1000) : "");
+  if (n < 1000000000) return terbilang(Math.floor(n / 1000000)) + " juta" + (n % 1000000000 > 0 ? " " + terbilang(n % 1000000) : "");
   
   return "";
 }
